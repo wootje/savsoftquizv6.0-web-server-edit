d3ed7c890d755405f1062b8f17457661a1b24ed2
wootje <wootje-inbox@outlook.com>
2023-01-28 04:38:31 +0100

    Update custom.js

--- application/dist/js/custom.js
+++ application/dist/js/custom.js
@@ -1,5 +1,5 @@
-var api_base_url="https://YOUR-DOMAINNAME/api/public/";
-var api_site_url="https://YOUR-DOMAINNAME/api/public/index.php/";
+var api_base_url="/api/public/";
+var api_site_url="/api/public/index.php";
 // 0 for disabled 1 for enabled
 var debug_mode=0;
 var user_data;
